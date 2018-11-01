1c932bd8a308d57bd14ae132ab5a679c4ff9a31b
Mr-Dangerous <39747022+Mr-Dangerous@users.noreply.github.com>
2018-11-01 06:34:10 -0400

    added the simple symbols puzzle

---
+++ src/SimpleSymbols/SimpleSymbols.kt
@@ -0,0 +1,20 @@
+package SimpleSymbols
+
+/* have the function SimpleSymbols(str) take the str parameter
+being passed and determine if it is an acceptable sequence by
+either returning the string true or false. The str parameter
+will be composed of + and = symbols with several letters between
+them (ie. ++d+===+c++==a) and for the string to be true each
+letter must be surrounded by a + symbol. So the string to the
+left would be false. (because of the 'a' at the end)
+The string will not be empty and will have at least one letter. */
+
+
+fun simpleSymbol(str: String){
+
+
+}
+
+fun main(args: Array<String>) {
+    println("The string follows the rules: " + simpleSymbol("+a+======+b+"))
+}
