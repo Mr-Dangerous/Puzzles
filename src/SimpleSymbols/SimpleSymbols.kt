9e5b98af883282bff8b3ebf39b6a46302352b881
Mr-Dangerous <39747022+Mr-Dangerous@users.noreply.github.com>
2018-11-01 08:33:12 -0400

    completed Simple Symbols

--- src/SimpleSymbols/SimpleSymbols.kt
+++ src/SimpleSymbols/SimpleSymbols.kt
@@ -1,5 +1,7 @@
 package SimpleSymbols
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 /* have the function SimpleSymbols(str) take the str parameter
 being passed and determine if it is an acceptable sequence by
 either returning the string true or false. The str parameter
@@ -10,11 +12,32 @@ left would be false. (because of the 'a' at the end)
 The string will not be empty and will have at least one letter. */
 
 
-fun simpleSymbol(str: String){
+fun simpleSymbol(str: String): Boolean{
+    var returnValue = false
+    var returnLock = false
+    var conditionMet = false
+    val charArray = str.toCharArray()
+    for ((i) in charArray.withIndex()) {
 
-
+        if (charArray.elementAt(i).isLetter() && (i > 0) && (i < charArray.size)) {
+            if ((charArray.elementAt(i - 1) == '+') && (charArray.elementAt(i+1) == '+')) {
+                conditionMet = true
+            }
+        }
+        if (conditionMet && !returnLock){
+            returnValue = true
+        }
+        if (!conditionMet && returnLock){
+            returnValue = false
+            returnLock = true
+        } else {
+            continue
+        }
+    }
+    return returnValue
 }
 
 fun main(args: Array<String>) {
-    println("The string follows the rules: " + simpleSymbol("+a+======+b+"))
+    println("The string follows the rules: " + simpleSymbol("==+a+======+b+=="))
+    println("This string follows the rules: " + simpleSymbol("efde+====dfdf"))
 }
